88be8de5122117220efe260b2bfad5498f86c740
Aman Bhinder <amanbhinder635@gmail.com>
2020-08-09 12:10:19 -0400

    This is the fifth commit from local master

--- FirstClass.java
+++ FirstClass.java
@@ -7,14 +7,12 @@ public class FirstClass {
 		System.out.println("This is the second commit from local master");
 		System.out.println("The third commit from local master");
 		System.out.println("This is the first commit on remote repo");
-<<<<<<< HEAD
+
         System.out.println("This is the fourth commit from local master");
-=======
-		
-		
+
 		System.out.println("This is the second commit from remote repo");
 
->>>>>>> ea44fba1a145e7de76f4ae97527bd56c006de4c0
+
 	}
 
 }
